a6b209bfe3d70bcf48a77a689281a35ac27421e8
yko@hist.co.kr <yko@hist.co.kr>
2023-10-20 14:05:36 +0900

    frontend first commit

--- crewpnr/frontend/src/main.js
+++ crewpnr/frontend/src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Vue3EasyDataTable from 'vue3-easy-data-table';
-import 'vue3-easy-data-table/dist/style.css';
 
 const app = createApp(App)
 
 app.use(router)
-app.component('EasyDataTable', Vue3EasyDataTable);
 
 app.mount('#app')
