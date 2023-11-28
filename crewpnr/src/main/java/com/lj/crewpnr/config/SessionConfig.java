2ff7ce94c67c3a553db5a6a09be62d9554c41e65
yko@hist.co.kr <yko@hist.co.kr>
2023-11-28 13:06:21 +0900

    세션 타임아웃 조정

--- crewpnr/src/main/java/com/lj/crewpnr/config/SessionConfig.java
+++ crewpnr/src/main/java/com/lj/crewpnr/config/SessionConfig.java
@@ -12,7 +12,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds=30)
+@EnableRedisHttpSession
 public class SessionConfig {
 
     @Value("${spring.redis.host}")
